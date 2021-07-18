2b08a481b914e47c74b70aff366d0bad6a3b1175
Lucas <lucasmbrute614@gmail.com>
2021-07-18 16:25:29 -0300

    Portfólio

--- Portfólio/assets/js/script.js
+++ Portfólio/assets/js/script.js
@@ -1,11 +1,51 @@
 let inputNome = document.querySelector('#nome')
+let inputEmail = document.querySelector('#email')
+let textareaMensagem = document.querySelector('#mensagem')
+let button = document.querySelector('#enviar')
 
-inputNome.addEventListener('keyup', () =>{
-    
-    if(inputNome.value.lenght <2){
-        console.log('Nome inválido')
+
+inputNome.addEventListener('keyup',()=>{
+
+    if(inputNome.value.length <2){
+        inputNome.style.borderColor = "red"
+    } else {
+     inputNome.style.borderColor = "green"
+    }
+
+})
+
+inputEmail.addEventListener("keyup", ()=>{
+
+    if(inputEmail.value.indexOf('@') == -1 || inputEmail.value.indexOf('.') == -1  ){
+
+        inputEmail.style.borderColor = 'red'
 
     } else {
-        console.log('Nome válido')
-    }      
+     inputEmail.style.borderColor = 'green'
+    }
+
+})
+
+textareaMensagem.addEventListener('keydown', () =>{
+
+    if(textareaMensagem.value.length <=100){
+
+        textareaMensagem.style.borderColor = 'green'
+
+    }else{
+        textareaMensagem.style.borderColor = 'red'
+    }
+
+
+})
+
+button.addEventListener('click', ()=>{
+
+    if(inputEmail.style.borderColor == 'green' && inputNome.style.borderColor == "green"&& textareaMensagem.style.borderColor == 'green'){
+        alert("Formulário enviado com sucesso!")
+    }  else{
+        alert('Preencha todas as informações corretamente')
+    }
+
+
 })
