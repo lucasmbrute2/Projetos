156aa95e52dc2e9e4a3dd69ae37297e65ed5f19c
Lucas <lucasmbrute614@gmail.com>
2021-07-19 17:24:39 -0300

    Projeto Humor

--- Jogo do humor/assets/js/script.js
+++ Jogo do humor/assets/js/script.js
@@ -2,7 +2,7 @@ let elementoButton = document.querySelector('#alterar')
 const elementoImg = document.querySelector('#imagem')
 const elementoNome = document.querySelector('#nomeNegrito')
 let elementoDesc = document.querySelector('#descricao')
-
+let nomeButton = document.querySelector('button')
 
 
 elementoButton.addEventListener('click', () =>{
@@ -13,12 +13,14 @@ elementoButton.addEventListener('click', () =>{
         elementoImg.src = "./assets/img/jr.jpg"
         elementoNome.innerText = "Desenvolvedor Jr"
         elementoDesc.innerText = "O código tem 300 linhas e quebrou."
+        nomeButton.innerText = "Ganhar Experiência"
         elementoButton.value = "segundo"
     } else if(elementoButton.value == "segundo"){
 
         elementoImg.src = "./assets/img/pleno.jpg"
         elementoNome.innerText = "Desenvolvedor Pleno"
         elementoDesc.innerText = "O seus códigos melhoraram e, incrivelmente, você começa a ser reconhecido como programador pelo seu visual."
+        nomeButton.innerText = "Ganhar Experiência"
         elementoButton.value = "terceiro"
 
     } else if(elementoButton.value == 'terceiro'){
@@ -26,6 +28,7 @@ elementoButton.addEventListener('click', () =>{
         elementoImg.src = "./assets/img/senior.png"
         elementoNome.innerText = "Desenvolvedor Senior"
         elementoDesc.innerText = "Você chegou no ápice."
+        nomeButton.innerText = "Voltar ao início do ciclo"
         elementoButton.value = "quarto"
     
     }
@@ -33,7 +36,8 @@ elementoButton.addEventListener('click', () =>{
 
         elementoImg.src = "./assets/img/experiencia.jpeg"
         elementoNome.innerText = "Iniciante de TI"
-        elementoDesc.innerText = "Você está tentanto entrar no mercado de trabalho."
+        elementoDesc.innerText = "Você está tentando entrar no mercado de trabalho."
+        nomeButton.innerText = "Ganhar Experiência"
         elementoButton.value = "primeiro"
 
     }
