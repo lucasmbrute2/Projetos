38ea86160ca39a66ebdcf3faecaa7a79c2da9269
Lucas <lucasmbrute614@gmail.com>
2021-07-19 22:20:21 -0300

    Projeto jogo humor

--- Jogo do humor/assets/js/script.js
+++ Jogo do humor/assets/js/script.js
@@ -3,42 +3,42 @@ const elementoImg = document.querySelector('#imagem')
 const elementoNome = document.querySelector('#nomeNegrito')
 let elementoDesc = document.querySelector('#descricao')
 let nomeButton = document.querySelector('button')
-
+let cont = 1
 
 elementoButton.addEventListener('click', () =>{
 
-    if(elementoButton.value == "primeiro"){
-        
+    if(cont == 1){
         
         elementoImg.src = "./assets/img/jr.jpg"
         elementoNome.innerText = " Desenvolvedor Jr"
         elementoDesc.innerText = "O código tem 300 linhas e quebrou."
         nomeButton.innerText = "Ganhar Experiência"
-        elementoButton.value = "segundo"
-    } else if(elementoButton.value == "segundo"){
+        cont += 1
+        
+    } else if(cont == 2){
 
         elementoImg.src = "./assets/img/pleno.jpg"
         elementoNome.innerText = " Desenvolvedor Pleno"
         elementoDesc.innerText = "O seus códigos melhoraram e, incrivelmente, você começa a ser reconhecido como programador pelo seu visual."
         nomeButton.innerText = "Ganhar Experiência"
-        elementoButton.value = "terceiro"
+        cont += 1
 
-    } else if(elementoButton.value == 'terceiro'){
+    } else if(cont == 3){
 
         elementoImg.src = "./assets/img/senior.png"
         elementoNome.innerText = " Desenvolvedor Sênior"
         elementoDesc.innerText = "Você chegou no ápice."
         nomeButton.innerText = "Voltar ao início do ciclo"
-        elementoButton.value = "quarto"
+        cont += 1
     
     }
-    else{
+    else if(cont ==4){
 
         elementoImg.src = "./assets/img/experiencia.jpeg"
         elementoNome.innerText = " Iniciante de TI"
         elementoDesc.innerText = "Você está tentando entrar no mercado de trabalho."
         nomeButton.innerText = "Ganhar Experiência"
-        elementoButton.value = "primeiro"
+        cont = 1
 
     }
 
